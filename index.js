97e43af3b28fe84477f534a852b39a9ac58fa64f
Samyc2002 <2000200402@iitdh.ac.in>
2022-09-02 01:02:35 +0530

    Added video to audio convertion

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const extractAudio = require('ffmpeg-extract-audio')
 
 const main = async () => {
+  // This extracts the audio from the video
   await extractAudio({
+    // this is the input video file in mp4
     input: 'assets/video_recording.mp4',
+    // this is the output audio file in mp3
     output: 'assets/converted.mp3'
   })
 }
